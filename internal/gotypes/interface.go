7efa5ea4cf45de343cfe3c1a894ee13aa7557189
Alice Qio <qioalice@gmail.com>
2020-01-06 16:57:26 +0300

    internal/gotypes: Added new type Interface - a way Golang represents interface{} internally

---
+++ internal/gotypes/interface.go
@@ -0,0 +1,14 @@
+// Copyright © 2019. All rights reserved.
+// Author: Ilya Yuryevich.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
+package gotypes
+
+import "unsafe"
+
+// Interface represents what "interface{}" means in internal Golang parts.
+type Interface struct {
+	Type uintptr        // pointer to the type definition struct
+	Word unsafe.Pointer // pointer to the value
+}
