176b24d6c069981db7ed4ac9c3c1ed8f90ababaf
Alice Qio <qioalice@gmail.com>
2020-01-14 15:20:51 +0300

    generators/randstrgen: Added random string generator based by length (useful in password generating)

---
+++ generators/randstrgen/randstrgen.go
@@ -0,0 +1,52 @@
+package randstrgen
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	charSetLetters = `abcdefghijklmnopqrstuvwxyz`
+	charSetDigits  = `1234567890`
+	charSetAll     = charSetLetters + charSetDigits
+)
+
+//
+var r *rand.Rand
+
+//
+func init() {
+	r = rand.New(rand.New(rand.NewSource(99)))
+	r.Seed(time.Now().UTC().Unix())
+}
+
+//
+func genWithLenFrom(charSet string, n int) string {
+
+	if n <= 0 {
+		return ""
+	}
+
+	res := make([]byte, n)
+
+	for i := 0; i < n; i++ {
+		res[i] = charSet[r.Intn(len(charSet))]
+	}
+
+	return string(res)
+}
+
+//
+func WithLen(n int) string {
+	return genWithLenFrom(charSetAll, n)
+}
+
+//
+func WithLenOnlyLetters(n int) string {
+	return genWithLenFrom(charSetLetters, n)
+}
+
+//
+func WithLenOnlyNumbers(n int) string {
+	return genWithLenFrom(charSetDigits, n)
+}
---
+++ generators/randstrgen/randstrgen_test.go
@@ -0,0 +1,23 @@
+package randstrgen
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func doTest(t *testing.T, n int) {
+	str := WithLen(n)
+	assert.Len(t, str, n)
+	fmt.Println(str)
+}
+
+func TestWithLen(t *testing.T) {
+	doTest(t, 10)
+	doTest(t, 10)
+	doTest(t, 10)
+	doTest(t, 10)
+	doTest(t, 10)
+	doTest(t, 10)
+}
