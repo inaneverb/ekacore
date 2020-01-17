300e8f40c4274de896e1122c323a23b7de9a6553
Alice Qio <qioalice@gmail.com>
2020-01-18 01:23:15 +0300

    log: Added Logger type, it's private part, finishers

---
+++ log/logger.go
@@ -0,0 +1,153 @@
+// Copyright © 2019. All rights reserved.
+// Author: Ilya Yuryevich.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
+package log
+
+// Logger is used to generate and write log messages.
+//
+// You can instantiate as many your own loggers with different behaviour,
+// different contexts, as you want. But also you can just use package level logger,
+// modernize and configure it the same way as any instantiated Logger object.
+//
+// Inheritance.
+//
+// Remember!
+// No one func or method does not change the current object.
+// They always creates and returns a copy of the current object with applied
+// your changes except in cases in which the opposite is not explicitly indicated.
+//
+// Of course you can chain all setters and then call log message generator, like this:
+//
+// 		log := Package("main")
+// 		log.With("key", "value").Warn("It's dangerous!")
+//
+// But in that case, after finishing execution of second line,
+// 'log' variable won't contain add field "key" and group "group".
+// But package name "main" will contain.
+// Want a different behaviour and want to have Logger with these fields?
+// No problem, save generated Logger object:
+//
+// 		log := Package("main")
+// 		log = log.Group("group").With("key", "value")
+// 		log.Warn("It's dangerous!")
+//
+// Because of all finishers (methods that actually writes a log message, e.g:
+// Debug, Debugf, Debugw, Warn, Warnf, Warnw, etc...) also returns a Logger
+// object they uses to generate log Entry, you can save it too, and finally
+// it's the same as in the example above:
+//
+// 		log := Package("main")
+// 		log = log.Group("group").With("key", "value").Warn("It's dangerous!")
+//
+// but it's strongly not recommended to do so, because it made code less clear.
+//
+// There are 5 Logger constructors:
+//
+// 		Package(packageName, options...)
+// 		Func(funcName, options...)
+// 		Class(className, options...)
+// 		Method(className, methodName, options...)
+// 		New(options...)
+//
+// You can instantiate Logger object using any of constructor listed above.
+// First four are used to create Logger object that binds to some Golang entity,
+// and their output will contain field with 'sys.func' key and your passed value.
+//
+// You can combine them then to explicitly create an exactly Logger you want:
+// E.g.: Method(className, methodName) == Class(className).Method(methodName).
+// In that case 'sys.func' will have this value: 'className.methodName'.
+//
+// And the fifth creates a common regular Logger object, but it will contain
+// 'sys.func' field also. Because there is auto-generation reflect information
+// by default (based by stacktrace).
+// You can disable this behaviour applying 'Options.EnableCallerInfo(false)'
+// option to Logger's constructor or using 'Apply' method.
+type Logger struct {
+
+	// Core copies much less times than Entry.
+	// Core contains log messages destinations, auxiliary functions, log format,
+	// and something like this.
+	core *core
+
+	// Entry is what log message is.
+	// Entry is it's stacktrace, caller info, timestamp, level, message, group,
+	// flags, etc.
+	entry *Entry
+}
+
+// Apply overwrites current Logger's copy behaviour by provided reasons.
+// Read more: Options.
+func (l *Logger) Apply(options ...interface{}) (copy *Logger) {
+
+	if len(options) == 0 || !l.canContinue() {
+		return l
+	}
+	return l.apply(options)
+}
+
+// Package sets current Logger's copy package name.
+// Zeroes Logger's function name or Logger's class and method names.
+// Disables caller auto-generation.
+func (l *Logger) Package(packageName string) (copy *Logger) {
+
+	if packageName == "" || !l.canContinue() {
+		return l
+	}
+	return l.derive(false).entry.setPackageName(packageName).l
+}
+
+// Func sets current Logger's copy function name.
+// Zeroes Logger's class and method names. Disables auto generating caller.
+func (l *Logger) Func(funcName string) (copy *Logger) {
+
+	if funcName == "" || !l.canContinue() {
+		return l
+	}
+	return l.derive(false).entry.setFuncName(funcName).l
+}
+
+// Class sets current Logger's copy class name.
+// Zeroes logger's function and method names. Disables auto generating caller.
+func (l *Logger) Class(className string) (copy *Logger) {
+
+	if className == "" || !l.canContinue() {
+		return l
+	}
+	return l.derive(false).entry.setClassName(className).l
+}
+
+// Method sets current Logger's copy method name.
+// Zeroes logger's function name. Disables auto generating caller.
+//
+// If class name isn't set, there is the same behaviour as Func.
+func (l *Logger) Method(methodName string) (copy *Logger) {
+
+	if methodName == "" || !l.canContinue() {
+		return l
+	}
+	return l.derive(false).entry.setMethodName(methodName).l
+}
+
+// With adds fields to the current Logger's copy.
+//
+// You can pass both of explicit or implicit fields. Even both of named/unnamed
+// implicit fields, but names (keys) should be only string.
+// Neither string-like (fmt.Stringer) nor string-cast ([]byte). Only strings.
+func (l *Logger) With(fields ...interface{}) (copy *Logger) {
+
+	if len(fields) == 0 || !l.canContinue() {
+		return l
+	}
+	return l.derive(false).entry.with(fields, nil).l
+}
+
+// WithStrict an explicit fields to the current Logger's copy.
+func (l *Logger) WithStrict(fields ...Field) (copy *Logger) {
+
+	if len(fields) == 0 || !l.canContinue() {
+		return l
+	}
+	return l.derive(false).entry.with(nil, fields).l
+}
---
+++ log/logger_private.go
@@ -0,0 +1,202 @@
+// Copyright © 2019. All rights reserved.
+// Author: Ilya Yuryevich.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
+package log
+
+import "fmt"
+import "time"
+
+import "github.com/qioalice/gext/death"
+import "github.com/qioalice/gext/errors"
+
+// canContinue reports whether l is valid (not nil and has valid core and entry).
+func (l *Logger) canContinue() bool {
+
+	// core and Entry (if they're not nil) can not be invalid (internally)
+	// because they are created only by internal functions and they're private.
+	// So 3 nil checks are enough here and of course check ptr equal.
+
+	return l != nil &&
+		l.core != nil &&
+		l.entry != nil &&
+		l == l.entry.l
+}
+
+// checkErr returns l if err != nil, otherwise nil.
+// Make sure you have canContinue call before next ops over returned object.
+//
+// It's very useful at the method chaining.
+// If err == nil, nil will be returned. Any Logger's public does nothing
+// (w/o panicking) if Logger's receiver is nil.
+func (l *Logger) checkErr(err error) (this *Logger) {
+
+	if err == nil {
+		return nil
+	}
+	return l
+}
+
+// apply changes l's behaviour according with new rules described by passed args.
+func (l *Logger) apply(args []interface{}) (copy *Logger) {
+
+	integrators, options := parseOptions(args)
+	needToCloneCore := len(integrators) > 0
+
+	copy = l.derive(needToCloneCore).entry.apply(options).l
+	copy.core.extend(integrators)
+
+	return
+}
+
+// log starts and manages all processes that should be passed between
+// forming log message and writing it.
+//
+// There are 4 big things this method shall done:
+//
+// 1. Figure out what will be used as log message's body and construct it
+//    if necessary (wasn't done before):
+//
+//    - Uses 'format' as just string if len(args) == 0;
+//    - Uses 'format' as printf-like format string if it's contains printf verbs
+//      and len(args) > 0, and then uses args[:N] as printf values;
+//    - Uses err's message as log's string if 'format' == "" and len(args) == 0;
+//    - Tries to extract string (or something string-like, e.g. fmt.Sprinter)
+//      or Golang error from 'args[0]' and then use it in an one of four cases
+//      described above. In that case only 'args[1:]' are allowed to be processed then.
+//
+// 2. Figure out what fields should be attached to the log message,
+//    Convert implicit fields (both of named/unnamed) to the explicit
+//    (using Entry.parseLogArgs method);
+//
+//    It guarantees that only one of 'args' or 'explicitFields' provided
+//    at the same time.
+//    If there's printf message, only 'args[N:]' or 'args[N+1:]' uses as
+//    explicit/implicit args, where N - printf verbs.
+//    N or N+1 depends by whether 'format' or 'err' was provided
+//    as printf-like string, or it was extracted from 'args[0]'.
+//
+// 3. Adds caller and stacktrace (if it necessary and if it wasn't provided
+//    by gext.errors.Error) (using Entry.addCallerAndStacktrace method);
+//
+//    Uses gext.errors.Error.Stacktrace as stacktrace avoiding another one
+//    stacktrace generation procedure.
+//
+// 4. Finally write a message and call then death.Die() if it's fatal level.
+func (l *Logger) log(lvl Level, format string, err error, args []interface{}, explicitFields []Field) *Logger {
+
+	if !(l.canContinue() && l.core.levelEnabled(lvl)) {
+		return l
+	}
+
+	// empty messages are skipped by default, but who knows?
+	if format == "" && err == nil && len(args) == 0 &&
+		!l.entry.testFlag(bEntryFlagDontSkipEmptyMessages) {
+		return l
+	}
+
+	// At this code point Entry should be copied anyway even if it's empty message,
+	// even w/o body, even w/o fields, because of at least timestamp.
+	//
+	// But we don't need to copy an internal Logger's entry:
+	// There is no necessary to keep (and then return) Logger with modified entry.
+	// Because otherwise I guess it will lead to very unpredictable behaviour.
+	workTempEntry := l.entry.clone()
+
+	workTempEntry.Level = lvl
+	workTempEntry.Time = time.Now()
+
+	// if both of format and explicit error is empty, maybe first passed arg is err?
+	// then handle it later as explicit error
+	if format == "" && err == nil && len(args) > 0 {
+
+		if err_, ok := args[0].(error); ok {
+			err = err_ // will be handled later
+			args = args[1:]
+		}
+	}
+
+	switch {
+	// use explicit error as message's body and moreover use its stacktrace
+	// if it's led's package error
+	case format == "" && err != nil:
+
+		if gextErr := errors.Unwrap(err); gextErr != nil {
+			workTempEntry.StackTrace = gextErr.StackTrace
+		}
+		format = err.Error()
+
+	// maybe first arg is something like string (ducktypes)?
+	// if it so, use it as message's body
+	case format == "" && len(args) > 0:
+
+		if str, ok := args[0].(string); ok && str != "" {
+			format = str
+			args = args[1:]
+
+		} else if stringer, ok := args[0].(fmt.Stringer); ok && stringer != nil {
+			format = stringer.String()
+			args = args[1:]
+		}
+	}
+
+	workTempEntry.
+		parseLogArgs(format, args, explicitFields).
+		addCallerAndStacktrace()
+
+	if workTempEntry.beforeWrite != nil {
+		returned := workTempEntry.beforeWrite(workTempEntry)
+		if returned == nil || returned != workTempEntry {
+			goto exit
+		}
+	}
+
+	l.core.write(workTempEntry)
+
+exit:
+	switch {
+	case workTempEntry.Level.mustDie():
+		death.Die()
+	}
+
+	reuseEntry(workTempEntry)
+	return l
+}
+
+// derive returns a new Logger with at least clone Entry based on l's one
+// and probably with cloned core (also based on l's) depends on cloneCore arg.
+func (l *Logger) derive(cloneCore bool) (newLogger *Logger) {
+
+	// Entry clones any way because of that fact that entry stores ptr to parent.
+	// if we won't clone we'll have a two different loggers with the same entry's ptrs
+	// that are points to the only one (first or second) logger.
+
+	if cloneCore {
+		return new(Logger).setCore(l.core.clone()).setEntry(l.entry.clone())
+	} else {
+		return new(Logger).setCore(l.core).setEntry(l.entry.clone())
+	}
+}
+
+// setCore changes the l's core to the passed. It's just assignment no more.
+// Useful in method chaining.
+func (l *Logger) setCore(newCore *core) (this *Logger) {
+
+	// It guarantees that newCore != nil
+
+	l.core = newCore
+	return l
+}
+
+// setEntry changes the l's base entry to the passed. Also changes parent ptr
+// in newEntry to being pointed to the l. Useful in method chaining.
+func (l *Logger) setEntry(newEntry *Entry) (this *Logger) {
+
+	// It guarantees that newEntry != nil
+
+	l.entry = newEntry
+	l.entry.l = l
+
+	return l
+}
