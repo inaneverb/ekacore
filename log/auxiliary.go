f42e538ab744d195749351d10125546134a03177
Alice Qio <qioalice@gmail.com>
2020-01-21 15:59:48 +0300

    log: added new auxiliary functions

--- log/auxiliary.go
+++ log/auxiliary.go
@@ -5,6 +5,12 @@
 
 package log
 
+import (
+	"strconv"
+
+	"github.com/qioalice/gext/sys"
+)
+
 // printfHowMuchVerbs reports how much printf verbs 'format' has.
 //
 // Also fixes 'format' if it's has some incorrect things like e.g.
@@ -53,4 +59,117 @@ func printfHowMuchVerbs(format *string) (verbsCount int) {
 	}
 
 	return
+}
+
+// implicitUnnamedFieldName returns string "unnamed_xx" where xx is idx.
+// There is generated code to avoid 'strconv.Itoa' call for [0..32] 'idx' values.
+func implicitUnnamedFieldName(idx int) string {
+
+	if idx < 0 || idx > 32 {
+		return "unnamed_" + strconv.Itoa(idx)
+	}
+
+	// TODO: Rewrite with binary search? Need more micro optimizations!
+
+	switch idx {
+	case 0:
+		return "unnamed_00"
+	case 1:
+		return "unnamed_01"
+	case 2:
+		return "unnamed_02"
+	case 3:
+		return "unnamed_03"
+	case 4:
+		return "unnamed_04"
+	case 5:
+		return "unnamed_05"
+	case 6:
+		return "unnamed_06"
+	case 7:
+		return "unnamed_07"
+	case 8:
+		return "unnamed_08"
+	case 9:
+		return "unnamed_09"
+	case 10:
+		return "unnamed_10"
+	case 11:
+		return "unnamed_11"
+	case 12:
+		return "unnamed_12"
+	case 13:
+		return "unnamed_13"
+	case 14:
+		return "unnamed_14"
+	case 15:
+		return "unnamed_15"
+	case 16:
+		return "unnamed_16"
+	case 17:
+		return "unnamed_17"
+	case 18:
+		return "unnamed_18"
+	case 19:
+		return "unnamed_19"
+	case 20:
+		return "unnamed_20"
+	case 21:
+		return "unnamed_21"
+	case 22:
+		return "unnamed_22"
+	case 23:
+		return "unnamed_23"
+	case 24:
+		return "unnamed_24"
+	case 25:
+		return "unnamed_25"
+	case 26:
+		return "unnamed_26"
+	case 27:
+		return "unnamed_27"
+	case 28:
+		return "unnamed_28"
+	case 29:
+		return "unnamed_29"
+	case 30:
+		return "unnamed_30"
+	case 31:
+		return "unnamed_31"
+	case 32:
+		return "unnamed_32"
+	default:
+		return ""
+	}
+}
+
+// formCaller forms one string that will contain passed args with following
+// format (if arg is not empty).
+func formCaller(packageName, funcName, className string) string {
+
+	if funcName != "" || className != "" {
+		// there is no need 'methodName != ""' check, because
+		// setMethodName does not allow to set package name if class name is empty.
+		packageName += "."
+	}
+
+	switch {
+
+	case className != "" && funcName != "":
+		return packageName + className + "." + funcName
+
+	case funcName != "":
+		return packageName + funcName
+
+	case className != "":
+		return packageName + className + ".<?>"
+
+	default:
+		return packageName
+	}
+}
+
+// formCaller2 forms and returns a string by stack frame that contains caller's info.
+func formCaller2(frame sys.StackFrame) string {
+	return frame.Function
 }
