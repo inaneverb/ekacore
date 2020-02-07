47ef099ae58185235ad9605c1063a796d80a76d4
Alice Qio <qioalice@gmail.com>
2020-02-07 18:52:06 +0300

    log: Buffer functions - a way to write string to []byte with growing (if it's need)

--- log/auxiliary.go
+++ log/auxiliary.go
@@ -152,3 +152,29 @@ func formCaller2(frame sys.StackFrame) string {
 
 	return fn + " (" + file + ":" + strconv.Itoa(frame.Line) + ")"
 }
+
+// bufgr is buffer grow - a function that takes some buffer 'buf',
+// and checks whether it has at least 'required' free bytes. Returns 'buf' if it so.
+// Otherwise creates a new buffer, with X as a new capacity, where:
+//
+// 		X ~= 'cap(buf) * 1.5 + required', but can be more,
+//
+// and copies all data from 'buf' to a new buffer, then returns it.
+func bufgr(buf []byte, required int) []byte {
+
+	if cap(buf)-len(buf) >= required {
+		return buf
+	}
+	// https://github.com/golang/go/wiki/SliceTricks#extend
+	return append(buf, make([]byte, required+cap(buf)/2)...)[:len(buf)]
+
+	// TODO: Maybe strict and guarantee that new cap === required + cap(buf) * 1.5?
+	//  ATM Golang may reserve a few bytes for internal prediction purpose I guess.
+}
+
+// bufw writes 'text' to 'buf', growing 'buf' if it's need to write 'text'.
+// Returns grown 'buf' (if it has been grown) or originally 'buf'.
+// So, it's recommend to use it func like Golang's one 'append'.
+func bufw(buf []byte, text string) []byte {
+	return append(bufgr(buf, len(text)), text...)
+}
