edd6ffc020b0b5152fd8c8f0055d9778d78a7a9c
Alice Qio <qioalice@gmail.com>
2020-07-07 13:09:36 +0300

    v2.0-rc: ekamath: Added aliases for fast min-max type dependent comparision

---
+++ ekamath/min_max.go
@@ -0,0 +1,148 @@
+// Copyright © 2020. All rights reserved.
+// Author: Ilya Stroy.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
+package ekamath
+
+func MinI(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinI8(a, b int8) int8 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinI16(a, b int16) int16 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinI32(a, b int32) int32 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinI64(a, b int64) int64 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinU(a, b uint) uint {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinU8(a, b uint8) uint8 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinU16(a, b uint16) uint16 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinU32(a, b uint32) uint32 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+func MinU64(a, b uint64) uint64 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func MaxI(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxI8(a, b int8) int8 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxI16(a, b int16) int16 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxI32(a, b int32) int32 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxI64(a, b int64) int64 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxU(a, b uint) uint {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxU8(a, b uint8) uint8 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxU16(a, b uint16) uint16 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxU32(a, b uint32) uint32 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func MaxU64(a, b uint64) uint64 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
