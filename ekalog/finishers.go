1831745a2b030c2be5ad9834dfbeb13df0fbdc00
Alice Qio <qioalice@gmail.com>
2020-08-04 14:22:15 +0300

    v2.3-wip: ekalog: Removed unnecessary finishers

--- ekalog/finishers.go
+++ ekalog/finishers.go
@@ -64,20 +64,7 @@ import (
 //
 // Not bad, huh?
 func Log(level Level, args ...interface{}) *Logger {
-
 	return baseLogger.log(level, "", nil, args, nil)
-}
-
-// LogStrict writes log message with desired 'level',
-// generating log's message using fmt.Sprint(args...).
-//
-// No explicit/implicit fields supporting, no printf style supporting,
-// no error supporting, no group supporting, no options supporting, ...
-//
-// ... and, as a conclusion, no reflections (usage of Golang 'reflect' package).
-func LogStrict(level Level, args ...interface{}) *Logger {
-
-	return baseLogger.log(level, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Logf writes log message with desired 'level',
@@ -85,164 +72,103 @@ func LogStrict(level Level, args ...interface{}) *Logger {
 // where N is a number of format's printf verbs, and uses args[N:] as explicit
 // or implicit fields (depends on what kind of fields are allowed).
 func Logf(level Level, format string, args ...interface{}) *Logger {
-
-	return baseLogger.log(level, format, nil, args, nil)
-}
-
-// LogfStrict is the same as just Logf but
-//
-// no explicit/implicit fields supporting, no group supporting,
-// no options supporting, ...
-//
-// ... and, as a conclusion, no reflections (usage of Golang 'reflect' package).
-func LogfStrict(level Level, format string, args ...interface{}) *Logger {
-
 	return baseLogger.log(level, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Logw writes log's message 'msg' with desired 'level', and passed implicit fields.
 func Logw(level Level, msg string, fields ...field.Field) *Logger {
+	return baseLogger.log(level, msg, nil, nil, fields)
+}
 
+func Logww(level Level, msg string, fields []field.Field) *Logger {
 	return baseLogger.log(level, msg, nil, nil, fields)
 }
 
 // Debug is the same as Log(Level.Debug, args...).
 // Read more: Entry.Log.
 func Debug(args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_DEBUG, "", nil, args, nil)
-}
-
-// DebugStrict is the same as LogStrict(Level.Debug, args...).
-// Read more: Entry.LogStrict.
-func DebugStrict(args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_DEBUG, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Debugf is the same as Logf(Level.Debug, format, args...).
 // Read more: Entry.Logf.
 func Debugf(format string, args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_DEBUG, format, nil, args, nil)
-}
-
-// DebugfStrict is the same as LogfStrict(Level.Debug, format, args...).
-// Read more: Entry.LogfStrict.
-func DebugfStrict(format string, args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_DEBUG, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Debugw is the same as Logw(Level.Debug, msg, fields...).
 // Read more: Entry.Logw.
 func Debugw(msg string, fields ...field.Field) *Logger {
+	return baseLogger.log(LEVEL_DEBUG, msg, nil, nil, fields)
+}
 
+func Debugww(msg string, fields []field.Field) *Logger {
 	return baseLogger.log(LEVEL_DEBUG, msg, nil, nil, fields)
 }
 
 // Info is the same as Log(Level.Info, args...).
 // Read more: Entry.Log.
 func Info(args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_INFO, "", nil, args, nil)
-}
-
-// InfoStrict is the same as LogStrict(Level.Info, args...).
-// Read more: Entry.LogStrict.
-func InfoStrict(args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_INFO, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Infof is the same as Logf(Level.Info, format, args...).
 // Read more: Entry.Logf.
 func Infof(format string, args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_INFO, format, nil, args, nil)
-}
-
-// InfofStrict is the same as LogfStrict(Level.Info, format, args...).
-// Read more: Entry.LogfStrict.
-func InfofStrict(format string, args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_INFO, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Infow is the same as Logw(Level.Info, msg, fields...).
 // Read more: Entry.Logw.
 func Infow(msg string, fields ...field.Field) *Logger {
+	return baseLogger.log(LEVEL_INFO, msg, nil, nil, fields)
+}
 
+func Infoww(msg string, fields []field.Field) *Logger {
 	return baseLogger.log(LEVEL_INFO, msg, nil, nil, fields)
 }
 
 // Warn is the same as Log(Level.Warn, args...).
 // Read more: Entry.Log.
 func Warn(args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_WARNING, "", nil, args, nil)
-}
-
-// WarnStrict is the same as LogStrict(Level.Warn, args...).
-// Read more: Entry.LogStrict.
-func WarnStrict(args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_WARNING, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Warnf is the same as Logf(Level.Warn, format, args...).
 // Read more: Entry.Logf.
 func Warnf(format string, args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_WARNING, format, nil, args, nil)
-}
-
-// WarnfStrict is the same as LogfStrict(Level.Warn, format, args...).
-// Read more: Entry.LogfStrict.
-func WarnfStrict(format string, args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_WARNING, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Warnw is the same as Logw(Level.Warn, msg, fields...).
 // Read more: Entry.Logw.
 func Warnw(msg string, fields ...field.Field) *Logger {
+	return baseLogger.log(LEVEL_WARNING, msg, nil, nil, fields)
+}
 
+func Warnww(msg string, fields []field.Field) *Logger {
 	return baseLogger.log(LEVEL_WARNING, msg, nil, nil, fields)
 }
 
 // Error is the same as Log(Level.Error, args...).
 // Read more: Entry.Log.
 func Error(args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_ERROR, "", nil, args, nil)
-}
-
-// ErrorStrict is the same as LogStrict(Level.Error, args...).
-// Read more: Entry.LogStrict.
-func ErrorStrict(args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_ERROR, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Errorf is the same as Logf(Level.Error, format, args...).
 // Read more: Entry.Logf.
 func Errorf(format string, args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_ERROR, format, nil, args, nil)
-}
-
-// ErrorfStrict is the same as LogfStrict(Level.Error, format, args...).
-// Read more: Entry.LogfStrict.
-func ErrorfStrict(format string, args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_ERROR, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Errorw is the same as Logw(Level.Error, msg, fields...).
 // Read more: Entry.Logw.
 func Errorw(msg string, fields ...field.Field) *Logger {
+	return baseLogger.log(LEVEL_ERROR, msg, nil, nil, fields)
+}
 
+func Errorww(msg string, fields []field.Field) *Logger {
 	return baseLogger.log(LEVEL_ERROR, msg, nil, nil, fields)
 }
 
@@ -250,31 +176,13 @@ func Errorw(msg string, fields ...field.Field) *Logger {
 // but also then calls death.Die(1).
 // Read more: Entry.Log.
 func Fatal(args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_FATAL, "", nil, args, nil)
-}
-
-// FatalStrict is the same as LogStrict(Level.Fatal, args...),
-// but also then calls death.Die(1).
-// Read more: Entry.LogStrict.
-func FatalStrict(args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_FATAL, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Fatalf is the same as Logf(Level.Fatal, format, args...),
 // but also then calls death.Die(1).
 // Read more: Entry.Logf.
 func Fatalf(format string, args ...interface{}) *Logger {
-
-	return baseLogger.log(LEVEL_FATAL, format, nil, args, nil)
-}
-
-// FatalfStrict is the same as LogfStrict(Level.Fatal, format, args...),
-// but also then calls death.Die(1).
-// Read more: Entry.LogfStrict.
-func FatalfStrict(format string, args ...interface{}) *Logger {
-
 	return baseLogger.log(LEVEL_FATAL, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
@@ -282,6 +190,9 @@ func FatalfStrict(format string, args ...interface{}) *Logger {
 // but also then calls death.Die(1).
 // Read more: Entry.Logw.
 func Fatalw(msg string, fields ...field.Field) *Logger {
+	return baseLogger.log(LEVEL_FATAL, msg, nil, nil, fields)
+}
 
+func Fatalww(msg string, fields []field.Field) *Logger {
 	return baseLogger.log(LEVEL_FATAL, msg, nil, nil, fields)
 }
--- ekalog/logger_finishers.go
+++ ekalog/logger_finishers.go
@@ -66,152 +66,103 @@ func (l *Logger) Log(level Level, args ...interface{}) (this *Logger) {
 // You can NOT add explicit/implicit fields using this method. And thus there is
 // no reflections (usage of Golang 'reflect' package).
 func (l *Logger) Logf(level Level, format string, args ...interface{}) (this *Logger) {
-	return l.log(level, format, nil, args, nil)
+	return l.log(level, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Logw writes log message 'msg' with desired 'level', and passed implicit fields.
 func (l *Logger) Logw(level Level, msg string, fields ...field.Field) (this *Logger) {
+	return l.log(level, msg, nil, nil, fields)
+}
 
+func (l *Logger) Logww(level Level, msg string, fields []field.Field) (this *Logger) {
 	return l.log(level, msg, nil, nil, fields)
 }
 
 // Debug is the same as Log(Level.Debug, args...).
 // Read more: Entry.Log.
 func (l *Logger) Debug(args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_DEBUG, "", nil, args, nil)
-}
-
-// DebugStrict is the same as LogStrict(Level.Debug, args...).
-// Read more: Entry.LogStrict.
-func (l *Logger) DebugStrict(args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_DEBUG, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Debugf is the same as Logf(Level.Debug, format, args...).
 // Read more: Entry.Logf.
 func (l *Logger) Debugf(format string, args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_DEBUG, format, nil, args, nil)
-}
-
-// DebugfStrict is the same as LogfStrict(Level.Debug, format, args...).
-// Read more: Entry.LogfStrict.
-func (l *Logger) DebugfStrict(format string, args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_DEBUG, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Debugw is the same as Logw(Level.Debug, msg, fields...).
 // Read more: Entry.Logw.
 func (l *Logger) Debugw(msg string, fields ...field.Field) (this *Logger) {
+	return l.log(LEVEL_DEBUG, msg, nil, nil, fields)
+}
 
+func (l *Logger) Debugww(msg string, fields []field.Field) (this *Logger) {
 	return l.log(LEVEL_DEBUG, msg, nil, nil, fields)
 }
 
 // Info is the same as Log(Level.Info, args...).
 // Read more: Entry.Log.
 func (l *Logger) Info(args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_INFO, "", nil, args, nil)
-}
-
-// InfoStrict is the same as LogStrict(Level.Info, args...).
-// Read more: Entry.LogStrict.
-func (l *Logger) InfoStrict(args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_INFO, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Infof is the same as Logf(Level.Info, format, args...).
 // Read more: Entry.Logf.
 func (l *Logger) Infof(format string, args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_INFO, format, nil, args, nil)
-}
-
-// InfofStrict is the same as LogfStrict(Level.Info, format, args...).
-// Read more: Entry.LogfStrict.
-func (l *Logger) InfofStrict(format string, args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_INFO, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Infow is the same as Logw(Level.Info, msg, fields...).
 // Read more: Entry.Logw.
 func (l *Logger) Infow(msg string, fields ...field.Field) (this *Logger) {
+	return l.log(LEVEL_INFO, msg, nil, nil, fields)
+}
 
+func (l *Logger) Infoww(msg string, fields []field.Field) (this *Logger) {
 	return l.log(LEVEL_INFO, msg, nil, nil, fields)
 }
 
 // Warn is the same as Log(Level.Warn, args...).
 // Read more: Entry.Log.
 func (l *Logger) Warn(args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_WARNING, "", nil, args, nil)
-}
-
-// WarnStrict is the same as LogStrict(Level.Warn, args...).
-// Read more: Entry.LogStrict.
-func (l *Logger) WarnStrict(args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_WARNING, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Warnf is the same as Logf(Level.Warn, format, args...).
 // Read more: Entry.Logf.
 func (l *Logger) Warnf(format string, args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_WARNING, format, nil, args, nil)
-}
-
-// WarnfStrict is the same as LogfStrict(Level.Warn, format, args...).
-// Read more: Entry.LogfStrict.
-func (l *Logger) WarnfStrict(format string, args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_WARNING, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Warnw is the same as Logw(Level.Warn, msg, fields...).
 // Read more: Entry.Logw.
 func (l *Logger) Warnw(msg string, fields ...field.Field) (this *Logger) {
+	return l.log(LEVEL_WARNING, msg, nil, nil, fields)
+}
 
+func (l *Logger) Warnww(msg string, fields []field.Field) (this *Logger) {
 	return l.log(LEVEL_WARNING, msg, nil, nil, fields)
 }
 
 // Error is the same as Log(Level.Error, args...).
 // Read more: Entry.Log.
 func (l *Logger) Error(args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_ERROR, "", nil, args, nil)
-}
-
-// ErrorStrict is the same as LogStrict(Level.Error, args...).
-// Read more: Entry.LogStrict.
-func (l *Logger) ErrorStrict(args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_ERROR, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Errorf is the same as Logf(Level.Error, format, args...).
 // Read more: Entry.Logf.
 func (l *Logger) Errorf(format string, args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_ERROR, format, nil, args, nil)
-}
-
-// ErrorfStrict is the same as LogfStrict(Level.Error, format, args...).
-// Read more: Entry.LogfStrict.
-func (l *Logger) ErrorfStrict(format string, args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_ERROR, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
 // Errorw is the same as Logw(Level.Error, msg, fields...).
 // Read more: Entry.Logw.
 func (l *Logger) Errorw(msg string, fields ...field.Field) (this *Logger) {
+	return l.log(LEVEL_ERROR, msg, nil, nil, fields)
+}
 
+func (l *Logger) Errorww(msg string, fields []field.Field) (this *Logger) {
 	return l.log(LEVEL_ERROR, msg, nil, nil, fields)
 }
 
@@ -219,31 +170,13 @@ func (l *Logger) Errorw(msg string, fields ...field.Field) (this *Logger) {
 // but also then calls death.Die(1).
 // Read more: Entry.Log.
 func (l *Logger) Fatal(args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_FATAL, "", nil, args, nil)
-}
-
-// FatalStrict is the same as LogStrict(Level.Fatal, args...),
-// but also then calls death.Die(1).
-// Read more: Entry.LogStrict.
-func (l *Logger) FatalStrict(args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_FATAL, fmt.Sprint(args...), nil, nil, nil)
 }
 
 // Fatalf is the same as Logf(Level.Fatal, format, args...),
 // but also then calls death.Die(1).
 // Read more: Entry.Logf.
 func (l *Logger) Fatalf(format string, args ...interface{}) (this *Logger) {
-
-	return l.log(LEVEL_FATAL, format, nil, args, nil)
-}
-
-// FatalfStrict is the same as LogfStrict(Level.Fatal, format, args...),
-// but also then calls death.Die(1).
-// Read more: Entry.LogfStrict.
-func (l *Logger) FatalfStrict(format string, args ...interface{}) (this *Logger) {
-
 	return l.log(LEVEL_FATAL, fmt.Sprintf(format, args...), nil, nil, nil)
 }
 
@@ -251,6 +184,9 @@ func (l *Logger) FatalfStrict(format string, args ...interface{}) (this *Logger)
 // but also then calls death.Die(1).
 // Read more: Entry.Logw.
 func (l *Logger) Fatalw(msg string, fields ...field.Field) (this *Logger) {
+	return l.log(LEVEL_FATAL, msg, nil, nil, fields)
+}
 
+func (l *Logger) Fatalww(msg string, fields []field.Field) (this *Logger) {
 	return l.log(LEVEL_FATAL, msg, nil, nil, fields)
 }
