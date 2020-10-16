1f5ea189bd3484a82622ea3cb86bf26dcd72f60d
Ilya Stroy <qioalice@gmail.com>
2020-10-16 12:08:27 +0300

    ekasys: Added bench for getStackFramePoints() func

--- ekasys/stacktrace_test.go
+++ ekasys/stacktrace_test.go
@@ -13,19 +13,19 @@ import (
 )
 
 // benchGetStackTraceCommonDepth aux bench func that starts
-// getStackTrace bench with specified 'depth' arg.
+// GetStackTrace bench with specified 'depth' arg.
 func benchGetStackTraceCommonDepth(b *testing.B, depth int) {
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		GetStackTrace(0, depth)
+		_ = GetStackTrace(0, depth)
 	}
 }
 
 // benchGetStackTraceSyntheticDepth increases stack depth level artificially
-// by 'createDepth' value and then starts getStackTrace bench with
+// by 'createDepth' value and then starts GetStackTrace bench with
 // specified 'depth' arg.
 func benchGetStackTraceSyntheticDepth(b *testing.B, depth, createDepth int) {
 
@@ -47,47 +47,148 @@ func benchGetStackTraceSyntheticDepth(b *testing.B, depth, createDepth int) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		f(0, depth)
+		_ = f(0, depth)
 	}
 }
 
-// Bench getStackTrace with 'skip' == 0, 'depth' == 1 on common stack.
-func BenchmarkGetStackTraceCommonDepth1(b *testing.B) {
+// Bench GetStackTrace with 'skip' == 0, 'depth' == 1 on common stack.
+func Benchmark_GetStackTrace_CommonDepth_1(b *testing.B) {
 	benchGetStackTraceCommonDepth(b, 1)
 }
 
-// Bench getStackTrace with 'skip' == 0, 'depth' == 10 on common stack.
-func BenchmarkGetStackTraceCommonDepth10(b *testing.B) {
+// Bench GetStackTrace with 'skip' == 0, 'depth' == 10 on common stack.
+func Benchmark_GetStackTrace_CommonDepth_10(b *testing.B) {
 	benchGetStackTraceCommonDepth(b, 10)
 }
 
-// Bench getStackTrace with 'skip' == 0, 'depth' == -1 (full depth)
+// Bench GetStackTrace with 'skip' == 0, 'depth' == -1 (full depth)
 // on common stack.
-func BenchmarkGetStackTraceCommonDepthFull(b *testing.B) {
+func Benchmark_GetStackTrace_CommonDepth_Full(b *testing.B) {
 	benchGetStackTraceCommonDepth(b, -1)
 }
 
-// Bench getStackTrace with 'skip' == 0, 'depth' == 1
+// Bench GetStackTrace with 'skip' == 0, 'depth' == 1
 // on artificially enlarged stack by 10.
-func BenchmarkGetStackTraceSyntheticDepth1(b *testing.B) {
+func Benchmark_GetStackTrace_SyntheticDepth_1_of_10(b *testing.B) {
 	benchGetStackTraceSyntheticDepth(b, 1, 10)
 }
 
-// Bench getStackTrace with 'skip' == 0, 'depth' == 10
+// Bench GetStackTrace with 'skip' == 0, 'depth' == 10
 // on artificially enlarged stack by 10.
-func BenchmarkGetStackTraceSyntheticDepth10(b *testing.B) {
+func Benchmark_GetStackTrace_SyntheticDepth_10_of_10(b *testing.B) {
 	benchGetStackTraceSyntheticDepth(b, 10, 10)
 }
 
-// Bench getStackTrace with 'skip' == 0, 'depth' == -1 (full depth)
+// Bench GetStackTrace with 'skip' == 0, 'depth' == -1 (full depth)
 // on artificially enlarged stack by 10.
-func BenchmarkGetStackTraceSyntheticDepthFull(b *testing.B) {
+func Benchmark_GetStackTrace_SyntheticDepth_Full_of_10(b *testing.B) {
 	benchGetStackTraceSyntheticDepth(b, -1, 10)
 }
 
-// Test getStackTrace with 'skip' == 0, 'depth' == 1,
+// Bench GetStackTrace with 'skip' == 0, 'depth' == 10
+// on artificially enlarged stack by 10.
+func Benchmark_GetStackTrace_SyntheticDepth_10_of_20(b *testing.B) {
+	benchGetStackTraceSyntheticDepth(b, 10, 20)
+}
+
+// Bench GetStackTrace with 'skip' == 0, 'depth' == -1 (full depth)
+// on artificially enlarged stack by 10.
+func Benchmark_GetStackTrace_SyntheticDepth_Full_of_20(b *testing.B) {
+	benchGetStackTraceSyntheticDepth(b, -1, 20)
+}
+
+// ---------------------------------------------------------------------------- //
+
+// benchGetStackFramePointsCommonDepth aux bench func that starts
+// getStackFramePoints bench with specified 'depth' arg.
+func benchGetStackFramePointsCommonDepth(b *testing.B, depth int) {
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = getStackFramePoints(0, depth)
+	}
+}
+
+// benchGetStackFramePointsSyntheticDepth increases stack depth level artificially
+// by 'createDepth' value and then starts getStackFramePoints bench with
+// specified 'depth' arg.
+func benchGetStackFramePointsSyntheticDepth(b *testing.B, depth, createDepth int) {
+
+	type tF func(int, int) []uintptr
+
+	wrapper := func(f tF) tF {
+		return func(i1 int, i2 int) []uintptr {
+			return f(i1, i2)
+		}
+	}
+
+	var f tF = getStackFramePoints
+
+	for i := 0; i < createDepth; i++ {
+		f = wrapper(f)
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = f(0, depth)
+	}
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == 1 on common stack.
+func Benchmark_getStackFramePoints_CommonDepth_1(b *testing.B) {
+	benchGetStackFramePointsCommonDepth(b, 1)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == 10 on common stack.
+func Benchmark_getStackFramePoints_CommonDepth_10(b *testing.B) {
+	benchGetStackFramePointsCommonDepth(b, 10)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == -1 (full depth)
+// on common stack.
+func Benchmark_getStackFramePoints_CommonDepth_Full(b *testing.B) {
+	benchGetStackFramePointsCommonDepth(b, -1)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == 1
+// on artificially enlarged stack by 10.
+func Benchmark_getStackFramePoints_SyntheticDepth_1_of_10(b *testing.B) {
+	benchGetStackFramePointsSyntheticDepth(b, 1, 10)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == 10
+// on artificially enlarged stack by 10.
+func Benchmark_getStackFramePoints_SyntheticDepth_10_of_10(b *testing.B) {
+	benchGetStackFramePointsSyntheticDepth(b, 10, 10)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == -1 (full depth)
+// on artificially enlarged stack by 10.
+func Benchmark_getStackFramePoints_SyntheticDepth_Full_of_10(b *testing.B) {
+	benchGetStackFramePointsSyntheticDepth(b, -1, 10)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == 10
+// on artificially enlarged stack by 10.
+func Benchmark_getStackFramePoints_SyntheticDepth_10_of_20(b *testing.B) {
+	benchGetStackFramePointsSyntheticDepth(b, 10, 20)
+}
+
+// Bench getStackFramePoints with 'skip' == 0, 'depth' == -1 (full depth)
+// on artificially enlarged stack by 10.
+func Benchmark_getStackFramePoints_SyntheticDepth_Full_of_20(b *testing.B) {
+	benchGetStackFramePointsSyntheticDepth(b, -1, 20)
+}
+
+// ---------------------------------------------------------------------------- //
+
+// Test GetStackTrace with 'skip' == 0, 'depth' == 1,
 // tests:
-// - getStackTrace returns slice with len == 1 (as depth)
+// - GetStackTrace returns slice with len == 1 (as depth)
 // - frame.Function contains current test name
 func TestGetStackTraceCommonDepth1(t *testing.T) {
 
@@ -98,9 +199,9 @@ func TestGetStackTraceCommonDepth1(t *testing.T) {
 		"wrong function name")
 }
 
-// Test getStackTrace with 'skip' == -3 (include hidden frames),
+// Test GetStackTrace with 'skip' == -3 (include hidden frames),
 // 'depth' == -1 (full depth) tests:
-// - getStackTrace returns slice with len >= 3
+// - GetStackTrace returns slice with len >= 3
 // (at least hidden frames were included to the output)
 // - first three returned frames have valid function names
 func TestGetStackTraceCommonDepthAbsolutelyFull(t *testing.T) {
@@ -120,6 +221,8 @@ func TestGetStackTraceCommonDepthAbsolutelyFull(t *testing.T) {
 
 	frames.Print(nil)
 }
+
+// ---------------------------------------------------------------------------- //
 
 type T struct{}
 
