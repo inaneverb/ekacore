b42a73dd2da55b91a5c7f2f6aeab9493d0b2fc31
Ilya Stroy <iyuryevich@pm.me>
2021-02-25 18:22:12 +0300

    ekalog: Removed feature to add new log levels, refactored according to syslog

--- ekalog/level.go
+++ ekalog/level.go
@@ -10,95 +10,74 @@ import (
 )
 
 type (
-	// Level represents the log message's level.
-	// All constants this type is package private (see below),
-	// but there is 'Levels' package-level struct, each field of that
-	// represents one log level.
+	// Level is the log message's severity level.
+	// There are 7 log levels the same as used in syslog.
+	//
+	// You can use log constants to determine
+	// which log entry will you receive in your Integrator.
+	//
+	// Keep in mind, logging using LEVEL_EMERGENCY cause calling ekadeath.Die(),
+	// after writing a log message.
+	//
+	// Read more:
+	// https://en.wikipedia.org/wiki/Syslog
+	//
 	Level uint8
 )
 
 //noinspection GoSnakeCaseUsage
 const (
-	// Levels represents the log message's levels.
-	// You can use these constants to determine which log entry will you receive
-	// in your Integrator.
-	//
-	// The int values represents each log level. Thus you can disable or enable all
-	// lt/lte/gt/gte log levels than your desired in Integrator. These values:
-	//   - Debug: 50,
-	//   - Info: 60,
-	//   - Warn: 70,
-	//   - Error: 80,
-	//   - Fatal: 90.
-	// It has step == 10 to allow you to determine up to 9 your own custom,
-	// intermediate log levels.
-
-	/* 50 */ LEVEL_DEBUG Level = 50 + 10*iota
-	/* 60 */ LEVEL_INFO
-	/* 70 */ LEVEL_WARNING
-	/* 80 */ LEVEL_ERROR
-	/* 90 */ LEVEL_FATAL
+	LEVEL_EMERGENCY Level = iota
+	LEVEL_ALERT
+	LEVEL_CRITICAL
+	LEVEL_ERROR
+	LEVEL_WARNING
+	LEVEL_NOTICE
+	LEVEL_INFO
+	LEVEL_DEBUG
 )
 
-// RegisterLevelName registers new log level's name that will be returned
-// by Level.String() method. Allows you to overwrite standard log level names
-// and name your own custom log levels. There is no-op if name is empty.
-func RegisterLevelName(level Level, name string) {
-	if name == "" {
-		return
-	}
-	names[level] = name
-	registeredNewLevels++
-}
-
-//
-func RegisteredCustomLevels() uint32 {
-	return registeredNewLevels
-}
-
-// MarkLevelAsFatal marks passed level as level when you write log with,
-// causes death.Die() (the same behaviour as standard Fatal handlers).
-//
-// You can undone it passing 'false' as second arg.
-// All next arg or 'true' as 1st are ignored.
-//
-// WARNING!
-// You can not mark standard levels as fatal.
-func MarkLevelAsFatal(level Level, enable ...bool) {
-
-	switch enable := !(len(enable) > 0 && !enable[0]); {
-	case enable && (level == LEVEL_DEBUG || level == LEVEL_INFO || level == LEVEL_WARNING ||
-		level == LEVEL_ERROR):
-		// can not mark standard levels
-
-	case level == LEVEL_FATAL:
-		// already marked or can not be unmarked
-
-	case enable && !level.mustDie():
-		// not marked yet
-		fatalLevels = append(fatalLevels, level)
-
-	case !enable:
-		for i, fatalLevel := range fatalLevels {
-			if level == fatalLevel {
-				fatalLevels = append(fatalLevels[:i], fatalLevels[i+1:]...)
-				break
-			}
-		}
-	}
-}
-
-// String returns a capitalized string representing the log level 'l'.
+// String returns a capitalized string of the current log level.
+// Returns an empty string if it's unexpected log level.
 func (l Level) String() string {
-	if name := names[l]; name != "" {
-		return name
-	} else {
-		return "Unknown"
+	switch l {
+	case LEVEL_EMERGENCY: return "Emergency"
+	case LEVEL_ALERT:     return "Alert"
+	case LEVEL_CRITICAL:  return "Critical"
+	case LEVEL_ERROR:     return "Error"
+	case LEVEL_WARNING:   return "Warning"
+	case LEVEL_NOTICE:    return "Notice"
+	case LEVEL_INFO:      return "Info"
+	case LEVEL_DEBUG:     return "Debug"
+	default:              return ""
 	}
 }
 
-// ToUpper returns an uppercase string representing the log level 'l'.
+// String3 returns a capitalized short-hand string of the current log level.
+// It has a length of 3 chars for all but LEVEL_EMERGENCY. Takes 5 for that.
+// Returns an empty string if it's unexpected log level.
+func (l Level) String3() string {
+	switch l {
+	case LEVEL_EMERGENCY: return "Emerg"
+	case LEVEL_ALERT:     return "Ale"
+	case LEVEL_CRITICAL:  return "Cri"
+	case LEVEL_ERROR:     return "Err"
+	case LEVEL_WARNING:   return "War"
+	case LEVEL_NOTICE:    return "Noe"
+	case LEVEL_INFO:      return "Inf"
+	case LEVEL_DEBUG:     return "Deb"
+	default:              return ""
+	}
+}
+
+// ToUpper returns an uppercase variant of String() call.
 func (l Level) ToUpper() string { return strings.ToUpper(l.String()) }
 
-// ToLower returns an lowercase string representing the log level 'l'.
+// ToLower returns a lowercase variant of String() call.
 func (l Level) ToLower() string { return strings.ToLower(l.String()) }
+
+// ToUpper3 returns an uppercase variant of String3() call.
+func (l Level) ToUpper3() string { return strings.ToUpper(l.String3()) }
+
+// ToLower3 returns an uppercase variant of String3() call.
+func (l Level) ToLower3() string { return strings.ToLower(l.String3()) }
