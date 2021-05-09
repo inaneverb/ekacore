0f8a04fa900814d223c9b6d65c443eb97f677dad
Ilya Stroy <qioalice@gmail.com>
2021-05-09 14:04:22 +0300

    ekamath: Added copyright

--- ekamath/flag.go
+++ ekamath/flag.go
@@ -1,3 +1,8 @@
+// Copyright © 2020-2021. All rights reserved.
+// Author: Ilya Stroy.
+// Contacts: qioalice@gmail.com, https://github.com/qioalice
+// License: https://opensource.org/licenses/MIT
+
 package ekamath
 
 type (
