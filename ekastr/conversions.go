f52c859165058a304a37e2d4f7f9f3e15bbc838f
Ilya Stroy <iyuryevich@pm.me>
2023-01-12 19:28:11 +0600

    ekastr: Moved ToString(), S2B(), B2S() to ekaunsafe

--- ekastr/conversions.go
+++ ekastr/conversions.go
@@ -6,35 +6,16 @@
 package ekastr
 
 import (
-	"reflect"
-	"unsafe"
+	"github.com/qioalice/ekago/ekaunsafe/v4"
 )
 
-// B2S converts byte slice to a string without memory allocation.
-func B2S(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+// FromBytes converts byte slice to a string without memory allocation.
+func FromBytes(b []byte) string {
+	return ekaunsafe.BytesToString(b)
 }
 
-// S2B converts string to a byte slice without memory allocation.
-//
-// WARNING! PANIC CAUTION!
-// It will lead to panic if you're going to modify a byte slice
-// that is received from the string literal. You should modify only strings
-// that are dynamically allocated (in heap), not in RO memory. Example:
-//
-//		var b = S2B("string")
-//		b[0] = 0         // <-- panic here
-//	 b = append(b, 0) // but this will work, because of copy
-func S2B(s string) (b []byte) {
-
-	var sh = (*reflect.StringHeader)(unsafe.Pointer(&s))
-	var bh = (*reflect.SliceHeader)(unsafe.Pointer(&b))
-
-	bh.Data = sh.Data
-	bh.Len = len(s) // We need to ensure s is still alive
-	bh.Cap = len(s) // We need to ensure s is still alive
-
-	// https://groups.google.com/g/golang-nuts/c/Zsfk-VMd_fU
-
-	return b
+// ToBytes converts string to a byte slice without memory allocation.
+// String literals should be used only as read-only. Panic otherwise.
+func ToBytes(s string) []byte {
+	return ekaunsafe.StringToBytes(s)
 }
